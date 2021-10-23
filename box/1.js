028aec1315730b2f9dd5033608a3f489cd40b45e
Jacob Worrel <jacobworrel@gmail.com>
2021-10-23 12:40:03 -0700

    implement Box

--- box/1.js
+++ box/1.js
@@ -1,8 +1,25 @@
+const Box = (x) => ({
+  map: (f) => Box(f(x)),
+  fold: (f) => f(x),
+  toString: () => `Box(${x})`,
+});
+
+// const nextCharForNumberString = str => {
+//   const trimmed = str.trim();
+//   const number = parseInt(trimmed);
+//   const nextNumber = number + 1;
+//   return String.fromCharCode(nextNumber);
+// };
+
+const identity = x => x;
+
 const nextCharForNumberString = str => {
-  const trimmed = str.trim();
-  const number = parseInt(trimmed);
-  const nextNumber = number + 1;
-  return String.fromCharCode(nextNumber);
+  return Box(str)
+    .map(x => x.trim())
+    .map(trimmed => parseInt(trimmed))
+    .map(num => num + 1)
+    .map(String.fromCharCode)
+    .fold(identity);
 };
 
 const result = nextCharForNumberString("  64 ");

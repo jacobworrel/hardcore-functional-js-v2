b3649696166f61285682bb54617f8a4a1473b542
Jacob Worrel <jacobworrel@gmail.com>
2021-10-23 14:02:42 -0700

    complete either/1.js

--- either/1.js
+++ either/1.js
@@ -1,6 +1,39 @@
-const findColor = name =>
-  ({ red: "#ff4444", blue: "#3b5998", yellow: "#fff68f" }[name]);
+const Right = x =>
+  ({
+    chain: f => f(x),
+    map: f => Right(f(x)),
+    fold: (f, g) => g(x),
+    toString: () => `Right(${x})`
+  })
 
-const res = findColor("red");
+const Left = x =>
+  ({
+    chain: f => Left(x),
+    map: f => Left(x),
+    fold: (f, g) => f(x),
+    toString: () => `Left(${x})`
+  })
 
-console.log(res);
+const findColor = name => {
+  const found = { red: "#ff4444", blue: "#3b5998", yellow: "#fff68f" }[name];
+  return found ? Right(found) : Left('missing');
+};
+
+const color = findColor("red")
+    .map(x => x.toUpperCase())
+    .map(x => x.slice(1))
+    .fold(
+      () => 'no color!',
+      (color) => color
+    );
+
+const invalidColor = findColor("not a color")
+  .map(x => x.toUpperCase())
+  .map(x => x.slice(1))
+  .fold(
+    () => 'no color!',
+    (color) => color
+  );
+
+console.log(color);
+console.log(invalidColor);

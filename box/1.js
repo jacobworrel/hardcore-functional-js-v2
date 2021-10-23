ac1837349f0c447e09ddf7a753ba36590310a86b
Jacob Worrel <jacobworrel@gmail.com>
2021-10-23 12:48:09 -0700

    complete box/2.js

--- box/1.js
+++ box/1.js
@@ -1,26 +1,21 @@
-const Box = (x) => ({
-  map: (f) => Box(f(x)),
-  fold: (f) => f(x),
-  toString: () => `Box(${x})`,
-});
+const Box = require("./box");
 
-// const nextCharForNumberString = str => {
-//   const trimmed = str.trim();
-//   const number = parseInt(trimmed);
-//   const nextNumber = number + 1;
-//   return String.fromCharCode(nextNumber);
-// };
+const nextCharForNumberString_ = str => {
+  const trimmed = str.trim();
+  const number = parseInt(trimmed);
+  const nextNumber = number + 1;
+  return String.fromCharCode(nextNumber);
+};
 
 const identity = x => x;
 
-const nextCharForNumberString = str => {
-  return Box(str)
+const nextCharForNumberString = str =>
+  Box(str)
     .map(x => x.trim())
     .map(trimmed => parseInt(trimmed))
     .map(num => num + 1)
     .map(String.fromCharCode)
     .fold(identity);
-};
 
 const result = nextCharForNumberString("  64 ");
 
--- box/2.js
+++ box/2.js
@@ -2,11 +2,17 @@ const Box = require("./box");
 
 const first = xs => xs[0];
 
-const halfTheFirstLargeNumber = xs => {
+const halfTheFirstLargeNumber_ = xs => {
   const found = xs.filter(x => x >= 20);
   const answer = first(found) / 2;
   return `The answer is ${answer}`;
 };
+
+const halfTheFirstLargeNumber = (xs) =>
+  Box(xs)
+    .map(xs => xs.filter(x => x >= 20))
+    .map(xs => first(xs) / 2)
+    .fold(answer => `The answer is ${answer}`)
 
 const res = halfTheFirstLargeNumber([1, 4, 50]);
 console.log(res);

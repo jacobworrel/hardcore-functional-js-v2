fbf513c20b0af95ff72f30a62c97e43302a772d1
Jacob Worrel <jacobworrel@gmail.com>
2021-10-23 12:58:39 -0700

    add box exercises

---
+++ box/ex.js
@@ -0,0 +1,47 @@
+// https://codepen.io/drboolean/pen/poodxOm?editors=0010
+
+const Box = require("./box");
+
+// Exercise: Box
+// Goal: Refactor each example using Box
+// Keep these tests passing!
+// Bonus points: no curly braces
+
+// Ex1: Using Box, refactor moneyToFloat to be unnested.
+// =========================
+const moneyToFloat_ = str =>
+  parseFloat(str.replace(/\$/, ''))
+
+const moneyToFloat = str => Box(str).fold(str => str.replace(/\$/, ''));
+
+
+// Ex2: Using Box, refactor percentToFloat to remove assignment
+// =========================
+const percentToFloat_ = str => {
+  const float = parseFloat(str.replace(/\%/, ''))
+  return float * 0.0100
+}
+
+const percentToFloat = str =>
+  Box(str)
+    .map(str => str.replace(/\%/, ''))
+    .map(parseFloat)
+    .fold(float => float * 0.0100);
+
+
+
+
+
+// Ex3: Using Box, refactor applyDiscount (hint: each variable introduces a new Box)
+// =========================
+const applyDiscount_ = (price, discount) => {
+  const cents = moneyToFloat(price)
+  const savings = percentToFloat(discount)
+  return cents - (cents * savings)
+}
+
+const applyDiscount = (price, discount) =>
+  Box(price)
+    .map(moneyToFloat)
+    .map(cents => cents - (cents * Box(discount).fold(percentToFloat)))
+    .fold(x => x);
